3c03bff8a2945377fcd349c104ed0e02b52e7312
happymomentweb-source <happymomentweb@gmail.com>
2026-05-23 14:24:54 +0700

    3

--- Program.cs
+++ Program.cs
@@ -3,14 +3,47 @@ using Backend_dotNet.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
+// Enable OpenAPI documentation (Microsoft standard)
+builder.Services.AddOpenApi();
+
+// Enable Swashbuckle Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TestdbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// MapOpenApi provides the /openapi/v1.json endpoint
+app.MapOpenApi();
+
+// Enable Swagger UI at /swagger
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    options.RoutePrefix = "swagger";
+});
+
+app.UseCors();
 
 app.MapControllers();
 
